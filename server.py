bb949c727f0866b727a8827f843209d9eefe1843
johannakate <jkgriffin@ucdavis.edu>
2015-09-07 23:51:45 -0700

    date_time update

--- server.py
+++ server.py
@@ -22,11 +22,14 @@ def index():
 		key = int(item.id)
 		value = [item.number, str(item.date_time)]
 		data_dict[key] = value 
-
+		y_axis_number.append(str(item.number))
+		x_axis_date_time.append(item.date_time)
 		# print item.number
-	json_response = json.load(data_dict)
+	print x_axis_date_time
+	print y_axis_number
+	json_response = json.dumps(data_dict)
 	print type(json_response)
-	return render_template("/dashboard.html", data=data, data_json=data_dict)
+	return render_template("/dashboard.html", data_json=json_response, date_time=x_axis_date_time, number=y_axis_number)
 
 if __name__ == '__main__':
 	app.debug = True
