fc70879112a8a8077753d0b5a87ee9032aa69cc1
johannakate <jkgriffin@ucdavis.edu>
2015-09-07 23:14:24 -0700

    d3 data

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask_debugtoolbar import DebugToolbarExtension
 from model import connect_to_db, db, Stats
 from jinja2 import StrictUndefined
 from sqlalchemy import func 
-
+import json
 
 app = Flask(__name__)
 
@@ -15,18 +15,18 @@ def index():
 	"""Dashboard"""
 	data = Stats.query.all()
 	data_dict = {}
+	x_axis_date_time = []
+	y_axis_number = []
+	frequency = {}
 	for item in data:
-		key = str(item.id)
-		value = [item.number, item.date_time]
+		key = int(item.id)
+		value = [item.number, str(item.date_time)]
 		data_dict[key] = value 
+
 		# print item.number
-	print len(data_dict)
-	# print type(data) this is class model.Stats
-	date_time_array = db.session.query(Stats.number).all()
-	print type(date_time_array)
-	# for number in date_time_array:
-	# 	print number
-	return render_template("/dashboard.html", data=data)
+	json_response = json.load(data_dict)
+	print type(json_response)
+	return render_template("/dashboard.html", data=data, data_json=data_dict)
 
 if __name__ == '__main__':
 	app.debug = True
